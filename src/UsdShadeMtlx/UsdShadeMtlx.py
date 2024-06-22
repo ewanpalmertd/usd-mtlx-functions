82166c03ad917dd7214eda819bfbecf1d974d6bb
ewan <ejp.wrk@gmail.com>
2024-06-22 22:18:06 +0100

    created utils function and empty json

---
+++ src/UsdShadeMtlx/UsdShadeMtlx.py
@@ -0,0 +1,8 @@
+from pxr import Usd, Sdf, UsdShade
+
+class MtlxShade:
+
+    def __init__(self, layer, path:str, nodeType:str) -> None:
+        self.layer = layer
+        self.path  = path
+        self. nodeType = nodeType
