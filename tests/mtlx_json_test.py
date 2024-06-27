bdd7343bc2a4d52660a33c3fb9af7eb18e9f9058
ewan <ejp.wrk@gmail.com>
2024-06-27 23:10:20 +0100

    uncommented function

--- tests/mtlx_json_test.py
+++ tests/mtlx_json_test.py
@@ -16,57 +16,57 @@ DATA_TYPES = {
     "Int"      : Sdf.ValueTypeNames.Int
 }
 
-# def getPropertiesDictFromNode(node: hou.LopNode) -> Dict:
-#     """
-#     creates a dictionary containing node input and output parms for conversion to UsdShade shader
+def getPropertiesDictFromNode(node: hou.LopNode) -> Dict:
+    """
+    creates a dictionary containing node input and output parms for conversion to UsdShade shader
     
-#     :params:
-#     node : the node to create dictionary from
+    :params:
+    node : the node to create dictionary from
 
-#     :returns: 
-#     dictionary with keys {shader name, shader inputs, shader outputs}
-#     """
+    :returns: 
+    dictionary with keys {shader name, shader inputs, shader outputs}
+    """
 
-#     tmp_dictionary = ""
+    tmp_dictionary = ""
 
-#     parmTemplateList: List[hou.ParmTemplate] = []
-#     for parameter in node.parms():
-#         if parameter.name() == "signature": continue # signatue is a UsdShade incompatible houdini native parameter found on materialx nodes
-#         parmTemplateList.append(parameter.parmTemplate())
+    parmTemplateList: List[hou.ParmTemplate] = []
+    for parameter in node.parms():
+        if parameter.name() == "signature": continue # signatue is a UsdShade incompatible houdini native parameter found on materialx nodes
+        parmTemplateList.append(parameter.parmTemplate())
 
-#     parmTemplateList = list(set(parmTemplateList)) # removes duplicates from parmtuple parameters
+    parmTemplateList = list(set(parmTemplateList)) # removes duplicates from parmtuple parameters
 
-#     nodeProperties: Dict = {
-#         "name": "ND_standard_surface_surfaceshader", # absolute name for the time being
-#         "inputs" : {},
-#         "outputs": {} }
+    nodeProperties: Dict = {
+        "name": "ND_standard_surface_surfaceshader", # absolute name for the time being
+        "inputs" : {},
+        "outputs": {} }
     
-#     for parm in parmTemplateList:
-#         parmType:str = str(parm.type()).split(".")[-1]
+    for parm in parmTemplateList:
+        parmType:str = str(parm.type()).split(".")[-1]
 
-#         if parmType == "FolderSet": continue
-#         if parmType == "Toggle":
-#             parmType = "Int"
+        if parmType == "FolderSet": continue
+        if parmType == "Toggle":
+            parmType = "Int"
 
-#         parmName:str    = parm.name()
-#         parmLabel:str   = parm.label()
-#         parmLength: int = parm.numComponents()
+        parmName:str    = parm.name()
+        parmLabel:str   = parm.label()
+        parmLength: int = parm.numComponents()
 
-#         # need to distinduish Color3f and Vector3f data types
-#         if parmLength > 1:
-#             suffix: str = f"{parmLength}{parmType.split('.')[-1][0].lower()}"
-#             default = node.parmTuple(parmName).eval()
-#             parmType = f"Color{suffix}" if node.parm(f"{parmName}r") else f"Vector{suffix}"
+        # need to distinduish Color3f and Vector3f data types
+        if parmLength > 1:
+            suffix: str = f"{parmLength}{parmType.split('.')[-1][0].lower()}"
+            default = node.parmTuple(parmName).eval()
+            parmType = f"Color{suffix}" if node.parm(f"{parmName}r") else f"Vector{suffix}"
 
-#         else:
-#             default = node.evalParm(parmName)
+        else:
+            default = node.evalParm(parmName)
 
-#         nodeProperties["inputs"][parmName] = [default, parmType]
+        nodeProperties["inputs"][parmName] = [default, parmType]
 
-#     for output in node.outputNames():
-#         nodeProperties["outputs"][output] = "Token" # all materialx nodes come with output type as Token
+    for output in node.outputNames():
+        nodeProperties["outputs"][output] = "Token" # all materialx nodes come with output type as Token
 
-#     return nodeProperties
+    return nodeProperties
 
 def exportPropertiesToJson(propertiesDict: Dict) -> None:
     """
